f40857292828ad0bb309b59a2fbde15017e72b4f
Sameer Pal <120613581+Sameer-Pal@users.noreply.github.com>
2023-09-11 20:51:49 +0530

    Added solution - LeetHub

---
+++ Lucky Numbers - GFG/lucky-numbers.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def isLucky(self, n):
+        return self.help(n,2)
+    def help(self,n,counter):
+            
+            if n%counter==0:
+                return 0
+            if counter>n:
+                return 1
+            return self.help(n-n//counter,counter+1)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        n=int(input())
+        obj = Solution()
+        if obj.isLucky(n):
+            print(1)
+        else:
+            print(0)
+        
+# } Driver Code Ends
