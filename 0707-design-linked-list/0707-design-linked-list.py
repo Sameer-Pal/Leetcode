abf1d93cd2ee36c2a2e1e4616cf753998f806666
Sameer Pal <120613581+Sameer-Pal@users.noreply.github.com>
2024-02-14 11:47:03 +0530

    Time: 113 ms (66.31%), Space: 17.3 MB (92.16%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        temp = Node(val)
+        temp.next = self.head
+        self.head = temp
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        temp = Node(val)
+        if not self.head:
+            self.head = temp
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = temp
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            new_node = Node(val)
+            curr = self.head
+            for _ in range(index - 1):
+                curr = curr.next
+            new_node.next = curr.next
+            curr.next = new_node
+            self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        if index == 0:
+            self.head = self.head.next
+        else:
+            curr = self.head
+            for _ in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+        self.size -= 1
